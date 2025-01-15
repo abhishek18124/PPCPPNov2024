51b4960a46c2605fbdfc4d20a2d7916155234dca
Abhishek Srivastava <abhisheksrivastava@Abhisheks-MacBook-Air.local>
2025-01-15 23:58:08 +0530

    .

--- Lecture 09/018Functions_ModifyArray.cpp
+++ Lecture 09/018Functions_ModifyArray.cpp
@@ -7,7 +7,7 @@ int main() {
 	int arr[] = {10, 20, 30, 40, 50};
 	int n = sizeof(arr) / sizeof(int);
 
-	for(int i=0; i<n; i++) {
+	for (int i = 0; i < n; i++) {
 		cout << arr[i] << " ";
 	}
 
@@ -15,7 +15,7 @@ int main() {
 
 	// todo ...
 
-	for(int i=0; i<n; i++) {
+	for (int i = 0; i < n; i++) {
 		cout << arr[i] << " ";
 	}
 
