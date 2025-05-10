e5b5fb736fbec27ff5c08d3d844fa48503551fde
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2025-05-10 19:51:04 +0530

    Lecture 37

---
+++ Lecture 37/005LongestSubstringWithKUniqueCharacters.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int main() {
+
+	string s = "aabacbebebe";
+	int n = s.size();
+	int k = 3;
+
+	int i = 0; // to store the start of the window
+	int j = 0; // to store the end of the windw
+
+	int max_len = 0; // to track the length of the longest substring
+	// i.e. window that contain k unique characters
+
+	int uniqCnt = 0; // to keep track of no. of unique characters in the window
+
+	map<char, int> freqMap; // to store a  mapping  between characters
+	// present in a window & their frequencies
+
+	// use array as a map since each op will be O(1), with map<>
+	// each op. is O(logn)
+
+	while (j < n) {
+
+		// expand the window
+		freqMap[s[j]]++;
+		if (freqMap[s[j]] == 1) {
+			uniqCnt++;
+		}
+
+		// check for the violation of the window property
+		if (uniqCnt > k) {
+			// window property is violated therefore start shrinking
+			while (uniqCnt > k) {
+				freqMap[s[i]]--;
+				if (freqMap[s[i]] == 0) {
+					uniqCnt--;
+				}
+				i++;
+			}
+		}
+
+		// check for the validity of the window, and update the answer
+		if (uniqCnt == k) {
+			// you've found a valid window
+			max_len = max(max_len, j - i + 1);
+		}
+
+		// continue expansion of the window
+		j++;
+	}
+
+	cout << max_len << endl;
+
+	return 0;
+}
